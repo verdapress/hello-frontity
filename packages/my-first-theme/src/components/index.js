865a69f8eeff6cd4ccf41ef2d067d0cbb3614f79
Ted Osuch <d503@verdapress.uk>
2022-02-07 17:00:28 +0000

    Added CSS via Emotion

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,36 +1,119 @@
 // File: /packages/my-first-theme/src/components/index.js
 
-import React from "react"
-import { connect } from "frontity"
-import Link from "@frontity/components/link"
-import Switch from "@frontity/components/switch"
-import List from "./list"
-import Post from "./post"
+import React from "react";
+// import { connect } from "frontity" - double import?????
+import Link from "@frontity/components/link";
+import Switch from "@frontity/components/switch";
+import List from "./list";
+import Post from "./post";
+import Page from "./page";
+import { connect, Global, css, styled } from "frontity";
 
-const Root = ({ state }) => {
-  const data = state.source.get(state.router.link)
+
+const Root = ({ state, actions }) => {
+  const data = state.source.get(state.router.link);
 
   return (
     <>
-      <h1>Hello Frontity</h1>
-      <p>Current URL: {state.router.link}</p>
-      <nav>
-        <Link link="/">Home</Link>
-        <br />
-        <Link link="/page/2">More posts</Link>
-        <br />
-        <Link link="/about-us">About Us</Link>
-      </nav>
+      <Global
+        styles={css`
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+          html {
+            font-family: system-ui, Verdana, Arial, sans-serif;
+          }
+        `}
+      />
+
+      <Header isPostType={data.isPostType} isPage={data.isPage}>
+        <HeaderContent>
+          <h1>Frontity Workshop</h1>
+          { state.theme.isUrlVisible
+  ? <>Current URL: {state.router.link} {" "}
+    <button onClick={actions.theme.toggleUrl}>&#x3c; Hide URL</button></>
+  : <button onClick={actions.theme.toggleUrl}>Show URL &#x3e;</button>
+}
+          <Menu>
+            <Link link="/">Home</Link>
+            <br />
+            <Link link="/page/2">More posts</Link>
+            <br />
+            <Link link="/about-us">About Us</Link>
+          </Menu>
+        </HeaderContent>
+      </Header>
       <hr />
-      <main>
+      <Main>
         <Switch>
           <List when={data.isArchive} />
           <Post when={data.isPost} />
-          <Post when={data.isPage} />
+          <Page when={data.isPage} />
         </Switch>
-      </main>
+      </Main>
     </>
-  )
-}
+  );
+};
 
-export default connect(Root)
+export default connect(Root);
+
+const Header = styled.header`
+  background-color: #e5edee;
+  border-width: 0 0 8px 0;
+  border-style: solid;
+  border-color: ${ props => props.isPostType ? ( props.isPage ? 'lightsteelblue' : 'lightseagreen' ) : 'maroon'};
+
+
+  h1 {
+    color: #4a4a4a;
+  }
+`;
+const HeaderContent = styled.div`
+  max-width: 800px;
+  padding: 2em 1em;
+  margin: auto;
+`;
+const Main = styled.main`
+  max-width: 800px;
+  padding: 1em;
+  margin: auto;
+
+  img {
+    max-width: 100%;
+  }
+  h2 {
+    margin: 0.5em 0;
+  }
+  p {
+    line-height: 1.25em;
+    margin-bottom: 0.75em;
+  }
+  figcaption {
+    color: #828282;
+    font-size: 0.8em;
+    margin-bottom: 1em;
+  }
+`;
+const Menu = styled.nav`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+  & > a {
+    margin-right: 1em;
+    color: steelblue;
+    text-decoration: none;
+  }
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border: none;
+  color: #aaa;
+
+  :hover {
+    cursor: pointer;
+    color: #888;
+  }
+`
--- packages/my-first-theme/src/components/list.js
+++ packages/my-first-theme/src/components/list.js
@@ -1,25 +1,34 @@
 // File: /packages/my-first-theme/src/components/list.js
 
 import React from "react"
-import { connect } from "frontity"
+import { connect, styled } from "frontity"
 import Link from "@frontity/components/link"
 
 const List = ({ state }) => {
   const data = state.source.get(state.router.link)
 
   return (
-    <div>
+    <Items>
       {data.items.map((item) => {
         const post = state.source[item.type][item.id]
         return (
-          <Link key={item.id} link={post.link}>
+          <Link link={post.link} key={post.id}>
             {post.title.rendered}
-            <br />
           </Link>
         )
       })}
-    </div>
+    </Items>
   )
 }
+
+const Items = styled.div`
+  & > a {
+    display: block;
+    margin: 6px 0;
+    font-size: 1.2em;
+    color: steelblue;
+    text-decoration: none;
+  }
+`
 
 export default connect(List)
