894ac7c872b658565578d76477c9cae827209236
Ted Osuch <d503@verdapress.uk>
2022-02-07 10:30:59 +0000

    Added WP connection

--- packages/my-first-theme/src/components/index.js
+++ packages/my-first-theme/src/components/index.js
@@ -1,13 +1,36 @@
 // File: /packages/my-first-theme/src/components/index.js
 
 import React from "react"
+import { connect } from "frontity"
+import Link from "@frontity/components/link"
+import Switch from "@frontity/components/switch"
+import List from "./list"
+import Post from "./post"
 
-const Root = () => {
+const Root = ({ state }) => {
+  const data = state.source.get(state.router.link)
+
   return (
     <>
       <h1>Hello Frontity</h1>
+      <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link link="/">Home</Link>
+        <br />
+        <Link link="/page/2">More posts</Link>
+        <br />
+        <Link link="/about-us">About Us</Link>
+      </nav>
+      <hr />
+      <main>
+        <Switch>
+          <List when={data.isArchive} />
+          <Post when={data.isPost} />
+          <Post when={data.isPage} />
+        </Switch>
+      </main>
     </>
   )
 }
 
-export default Root
+export default connect(Root)
